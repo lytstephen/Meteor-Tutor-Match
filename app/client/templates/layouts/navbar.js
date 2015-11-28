d33df942f8a657af273a575f3fed79a3ee864f12
Stephen Leung <stephen@spacious.hk>
2015-11-28 08:53:09 +0800

    active nav

--- app/client/templates/layouts/navbar.js
+++ app/client/templates/layouts/navbar.js
@@ -3,3 +3,12 @@ Template.navbar.events({
     AccountsTemplates.logout();
   }
 });
+
+Template.navbar.helpers({
+  isActive: function(link) {
+    //alert(Router.current().route.path(this) + ':' + link);
+    var currentPath = Router.current().route.path(this).toString();
+    console.log(currentPath + ' : ' + link);
+    return link == currentPath ? 'active' : '';
+  }
+});
